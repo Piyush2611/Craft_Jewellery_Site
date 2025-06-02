8897bead2d2138bd2ecd42149b2bcfa49acf072c
Piyush Soni <piyushsoni261199@gmail.com>
2025-06-01 18:03:33 -0700

    add css

--- craft/src/components/HeroSection.jsx
+++ craft/src/components/HeroSection.jsx
@@ -15,7 +15,6 @@ const HeroSection = () => {
                         width: '58%',
                         height: { xs: 'auto', md: '60vh' },
                         overflow: 'hidden',
-                        border: '1px solid red',
                         display: 'flex',
                     }}
                 >
@@ -29,15 +28,24 @@ const HeroSection = () => {
                         }}
                     />
                 </Box>
-
-                <Box sx={{ p: 4, mr: 12, ml: 4 }}>
-                    <Typography variant="h4" component="h2" gutterBottom>
-                        Welcome to Our Store
+                <Box
+                    sx={{
+                        p: 4,
+                        maxWidth: 400,
+                        mx: 'auto',
+                        textAlign: 'center',
+                    }}
+                >
+                    <Typography variant="h4" component="h2" gutterBottom sx={{marginTop:'5px', marginBottom:'40px'}}>
+                        Welcome to LUXRIO
                     </Typography>
-                    <Typography variant="body1">
-                        Explore our exclusive collection of necklaces and rings.
+                    <Typography variant="body1"sx={{marginTop:'5px', lineHeight: 1.8}}>
+                        Elevate your style with our exclusive collection of handcrafted necklaces and rings. Each piece is designed to reflect timeless elegance, blending modern sophistication with classic beauty. Whether for a special occasion or everyday luxury, our jewelry brings brilliance and meaning to every moment.
                     </Typography>
                 </Box>
+
+
+
             </Box>
         </Box>
     );
