48754cb69a2f8144a2e604f28c5bc6a8e511dd9b
Piyush Soni <piyushsoni261199@gmail.com>
2025-06-01 19:52:53 -0700

    add the price and fix the Css

--- craft/src/components/HighClassJewelry.jsx
+++ craft/src/components/HighClassJewelry.jsx
@@ -7,32 +7,33 @@ import earings from '../assets/earings.jpg';
 
 const HighClassJewelry = () => {
   const items = [
-    { title: 'Chandelier', image: chandelierimage },
-    { title: 'Elegant Ring', image: elegantring },
-    { title: 'Classic Bracelet', image: braclate },
-    { title: 'Dangle Earings', image: earings },
+    { title: 'Chandelier', image: chandelierimage, price: '$150.00' },
+    { title: 'Elegant Ring', image: elegantring, price: '$99.00' },
+    { title: 'Classic Bracelet', image: braclate, price: '$130.00' },
+    { title: 'Diamond Stud', image: earings, price: '$180.00' },
   ];
+
   return (
-    <Box sx={{ textAlign: 'center', py: 6}}>
+    <Box sx={{ textAlign: 'center', py: 12 }}>
       {/* Headings Section */}
       <Box sx={{ mb: 6, mt: 3 }}>
         <Typography variant="subtitle2" sx={{ color: 'red', mb: 1 }}>
           FIND THE PERFECT
         </Typography>
 
-        <Typography variant="h3" sx={{ mb: 2 }}>
+        <Typography variant="h3" sx={{ mb: 2}}>
           HIGH CLASS JEWELLERY
         </Typography>
 
-        <Typography variant="subtitle2">
+        <Typography variant="subtitle2" sx={{ mb: 3 }}>
           Elevate your elegance with timeless pieces
         </Typography>
       </Box>
 
-      <Grid container spacing={3} justifyContent="center">
+      <Grid container spacing={13} justifyContent="center">
         {items.map((item, index) => (
           <Grid item xs={6} md={2} key={index}>
-            <Card sx={{ backgroundColor: '#111', color: 'white' }}>
+            <Card sx={{ backgroundColor: '#2a2a2a', color: 'white' }}>
               <CardMedia
                 component="img"
                 height="140"
@@ -42,13 +43,14 @@ const HighClassJewelry = () => {
               <CardContent>
                 <Typography variant="body1" align="center">{item.title}</Typography>
                 <Typography variant="body2" align="center" sx={{ color: 'red' }}>
-                  $125.00 – $145.00
+                  {item.price}
                 </Typography>
               </CardContent>
             </Card>
           </Grid>
         ))}
       </Grid>
+
 
     </Box>
 
